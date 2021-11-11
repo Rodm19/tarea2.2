7c1d4f06a063a045eeba3e950b635e3885283c2e
rodm19 <rm235124@gmail.com>
2021-11-11 14:32:57 -0400

    tarea2.2

---
+++ resources/views/formulario.blade.php
@@ -0,0 +1,7 @@
+<form action="/action_page.php">
+  <label for="fname">Nombre:</label>
+  <input type="text" id="lnombre" name="lnombre"><br><br>
+  <label for="lname">Edad:</label>
+  <input type="number" id="ledad" name="ledad"><br><br>
+  <input type="submit" value="Submit">
+</form>
